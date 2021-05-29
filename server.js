e5a36d22fb198232da4190f9f0609ba4249f8685
SilentGamelan <28412081+SilentGamelan@users.noreply.github.com>
2021-05-29 15:29:55 +0100

    Add basic https/websocket server code skeleton

---
+++ server.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express');
+const WebSocket = require('ws');
+const fs = require('fs');
+const https = require('https');
+
+
+var privateKey = fs.readFileSync("server.key", "utf8");
+var certificate = fs.readFileSync("server.cert", "utf8");
+var credentials = {key: privateKey, cert: certificate};
+
+const HTTPS_PORT = 443;
+const httpsServer = https.createServer(credentials, app);
+const wsServer = new WebSocket.Server({server: httpsServer});
+const app = express();
+
+let connectedClients = [];
+
+wsServer.on("connection", (ws, req)=> {
+    console.log("Connected via websockets");
+
+    ws.on("message", (data) => {
+        if(data.indexOf("WEB_CLIENT") !== -1) {
+            connectedClients.push(ws);
+            console.log("New WEB_CLIENT added: " + data.indexOf("WEB_CLIENT") + "/" + connectedClients.length);
+            return; 
+        }
+
+        connectedClients.forEach((ws, i) => {
+            if(connectedClients[i] == ws && ws.readState == ws.OPEN) {
+                ws.send(data);
+            } else {
+                connectedClients.splice(i, 1);
+            }
+        });
+    });
+
+    ws.on("error", (error) => {
+        console.error("Websocket Error - ", error);
+    });
+});
+
+app.use(express.static("."));
+app.get("/client", (req, res) => res.sendFile(path.resolve(__dirname, "./client.html")));
+httpsServer.listen(HTTPS_PORT, () => console.log('HTTPS server listening at ${HTTPS_PORT}'));
+
